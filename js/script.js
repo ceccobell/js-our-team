c9357b61feff9b4ad90e0741e9440c6bb8ea9b88
ceccobell <francescobell2001@gmail.com>
2024-07-10 14:34:25 +0200

    [UPD] milestone 1 done

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
 let team = [
     {
-        nome: "Wayne Barnett"
-        ruolo: "Founder & CEO"
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
         foto: "wayne-barnett-founder-ceo.jpg"
     },
     {
-        nome: "Angela Carrol"
-        ruolo: "Chief Editor"
+        nome: "Angela Carrol",
+        ruolo: "Chief Editor",
         foto: "angela-caroll-chief-editor.jpg"
     },
     {
-        nome: "Walter Gordon"
-        ruolo: "Office Manager"
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
         foto: "walter-gordon-office-manager.jpg"
     },
     {
-        nome: "Angela lopez"
-        ruolo: "Social Media Manager"
+        nome: "Angela lopez",
+        ruolo: "Social Media Manager",
         foto: "angela-lopez-social-media-manager.jpg"
     },
     {
-        nome: "Scott Estrada"
-        ruolo: "Developer"
+        nome: "Scott Estrada",
+        ruolo: "Developer",
         foto: "scott-estrada-developer.jpg"
     },
     {
-        nome: "Barbara ramos"
-        ruolo: "Graphic Designer"
+        nome: "Barbara ramos",
+        ruolo: "Graphic Designer",
         foto: "barbara-ramos-graphic-designer.jpg"
     }
 
 ]
+console.log(team)
